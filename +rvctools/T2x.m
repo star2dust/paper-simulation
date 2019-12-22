4702d35e7851bb5e373e47e6fb9fabfa478ef2ae
chu <wuchu9402@gmail.com>
2019-12-22 11:50:08 +0800

    update version

--- +rvctools/T2x.m
+++
@@ -1,6 +0,0 @@
-function x = T2x(T)
-% T: Homogeneous Transformation Matrix
-% x: [xyz rpy] in a row vector
-[~,t] = tr2rt(T);
-x = [t',tr2rpy(T)];
-end
--- getcurvia.m
+++
@@ -1,4 +0,0 @@
-function [y,dy] = getcurvia(yarray, dyarray, tarray, tnow)
-y = interp1(tarray,yarray,tnow); 
-dy = interp1(tarray,dyarray,tnow);
-end
